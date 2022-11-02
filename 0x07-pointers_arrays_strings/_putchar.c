d6e7a621658270ed64257cffc1145114c4ea27dc
diran-2 <diran2dave@yahoo.com>
2022-11-01 17:29:40 -0700

    Task 0

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,11 +1,9 @@
-#include "main.h"
 #include <unistd.h>
 
 /**
  * _putchar - writes the character c to stdout
  * @c: the character to print
  * Return: 1 on success
- * on error, -1 is returned anderrno is set appropraitely
  */
 int _putchar(char c)
 {
