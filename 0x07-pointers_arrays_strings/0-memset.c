caedf0320d1fc57b207c3a1e94cf5b226f2a6f43
diran-2 <diran2dave@yahoo.com>
2022-11-01 17:19:33 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,20 +2,21 @@
 
 /**
  * _memset - Fills the first n byte of the memory
- * area pointed to by s with the constant byte c
+ * area pointed to by s with the constant byte b
  * @s: a pointer to the memory area to be filled
- * @c: the character to fill the memory area with
+ * @b: the character to fill the memory area with
  * @n: the number of bytes to be filled
- * description _memset: over there
- * Return: a pointer to the filled memory area s
+ * Return: changed array with new value for n bytes
  */
-void *_memset(void *s, int c, size_t n)
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *memory = s, value = c;
+	int i = 0;
 
-	for (index = 0; index < n; index++)
-		memory[index] = value;
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
 
-	return (memory);
+	return (s);
 }
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,13 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- * Auth: david johnson
- * Description: Header file containing prototypes for all functions used in the last project
- */
-#include <stddef.h>
-
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -17,6 +10,7 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
-void *_memset(void *s, int c, size_t n)
+void *_memset(void *s, int c, size_t n);
+void set_string(char **s, char *to);
 
 #endif
